1b6e6832beb5f26d22b1a1818df3aa7ce0fbbe03
AngelG-JAPY <83625758+AngelG-JAPY@users.noreply.github.com>
2021-05-03 22:26:10 -0500

    Delete Reto2.py

--- Reto2.py
+++
@@ -1,8 +0,0 @@
-#Estamos interesados en comprar un nuevo teléfono móvil y en el escaparate de una tienda aparece que el móvil 
-# cuesta 420€. El problema es que si nos esperamos a comprarlo al día siguiente sufrirá un incremento 
-# porcentual del 20%. ¿Cuánto costará entonces el teléfono si nos esperamos?
-
-valor_sin_iva = 420
-valor_con_iva = valor_sin_iva + valor_sin_iva*0.20
-
-print("El valor con iva será de",valor_con_iva)
