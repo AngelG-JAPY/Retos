016db011564da8129975821df23c2730154458b2
AngelG-JAPY <83625758+AngelG-JAPY@users.noreply.github.com>
2021-05-03 22:26:16 -0500

    Delete Reto3.py

--- Reto3.py
+++
@@ -1,6 +0,0 @@
-#Sabemos que un Fidget Spinner da 147 vueltas por minuto ¿Cuántas vueltas dará en 640 segundos?
-# Para este ejercicio se desprecia el rozamiento con el aire.
-
-minutos = 640/60
-cantidad_de_vueltas = 147*minutos
-print("La cantidad de vueltas que dara será:",cantidad_de_vueltas)
